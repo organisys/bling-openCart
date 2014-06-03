11ca2a0c44c4886fea3a21e0adced6ad534631a0
Lucas Silveira <lucas@bling.com.br>
2014-06-03 15:57:17 -0300

    Update título aplicação

--- opencart-bling/admin/language/english/module/bling.php
+++ opencart-bling/admin/language/english/module/bling.php
@@ -21,12 +21,12 @@ $_['my_module_example'] = 'Example Extra Text';
 
 
 // Heading Goes here:
-$_['heading_title']    = 'My Module';
+$_['heading_title']    = 'Bling';
 
 
 // Text
 $_['text_module']      = 'Modules';
-$_['text_success']     = 'Success: You have modified module My Module!';
+$_['text_success']     = 'Success: You have modified module Bling!';
 $_['text_content_top']    = 'Content Top';
 $_['text_content_bottom'] = 'Content Bottom';
 $_['text_column_left']    = 'Column Left';
@@ -42,5 +42,5 @@ $_['entry_status']        = 'Status:';
 $_['entry_sort_order']    = 'Sort Order:';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify module My Module!';
+$_['error_permission'] = 'Warning: You do not have permission to modify module Bling!';
 ?>
